2f88e46829ce2577d19314c907aebb1d1da1303a
Alejandro Rodriguez <arodrig@alumni.stanford.edu>
2018-05-09 21:29:11 -0700

    Changed logo from body background to image

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    /*return (
+    return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src="/images/LOGO.png" id="background" />
       </div>
-    );*/
-    return null;
+    );
   }
 }
 
